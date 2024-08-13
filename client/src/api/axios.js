6bec90da69f4da6a22c4c16eef726c58b7601c2f
jaime chico loayza <jrchldev@gmail.com>
2024-08-13 18:39:16 -0500

    url cambio

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://listasks-front.vercel.app/api',
+    baseURL: 'https://listasks-back.vercel.app/api',
     withCredentials: true,
 })
 
